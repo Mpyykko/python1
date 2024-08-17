86775f4ad436772b7d95ac7a8dc5d924323c1cc8
Koijari <148049848+Koijari@users.noreply.github.com>
2024-08-17 17:58:45 +0300

    Lisäys tietovisa.py
    Toiminnan logiikka:
    -satunnaiset kysymykset
    -vastaukset satunnaisessa järjestyksessä
    -Ei samaa kysymystä kahta kertaa
    
    Taas lisätty kommenttina, josta helppo testata
    
    Puuttuu vielä:
    -Funktioiksi hajauttaminen
    -Pistekortti/palkintotaulu
    -Väärän ja oikean vastauksen logiikka

--- tietovisa.py
+++ tietovisa.py
@@ -45,18 +45,63 @@ if __name__ == '__main__':
 '''
 import random
 
+n = [0,1,2,3]
+m = []
 helpot_kysymykset = {
     1:['Helsinki', 'Halsinki', 'Hulsinki', 'Joku muu', 'Suomen pääkaupunki on?'],
     2:['Tapio Rautavaara', 'Reino Helismaa', 'Georg Otz', 'Kirka', 'Kuka oli tunnettu laulava keihäänheittäjä?'],
-    3:['Soisalo', 'Heisalo', 'Koisalo', 'Seisalo', 'Mikä suomalainen näyttelijä-ohjaaja Martti on sukuaan?']
+    3:['Soisalo', 'Heisalo', 'Koisalo', 'Seisalo', 'Mikä suomalainen näyttelijä-ohjaaja Martti on sukuaan?'],
+    4:['3,14', '3,41', '4,13', '1,34', 'Piin likiarvo on?'],
+    5:['Simeoni', 'Eero', 'Aapo', 'Lauri', 'Kuka oli kolmanneksi vanhin Jukolan veljeksistä?']
 }
 
-x = random.randint(1, len(helpot_kysymykset))
-oikea_vastaus = helpot_kysymykset[x][0]
-print(helpot_kysymykset[x][-1], helpot_kysymykset[x][0])
 
-if (oikea_vastaus):
-    print('pisteet kotiin!')
-else:
-    print('Ei tainnut olla')
+def visa_valikko():
+    x = random.randint(1, len(helpot_kysymykset))
+    for i in range(5):
+        random.shuffle(n)
+        while x in m:
+            x = random.randint(1, len(helpot_kysymykset))
+        m.append(x)
+        oljenkorsi1 = 1
+        oljenkorsi2 = 1
+        print()
+        while True:
+            try:
+                print(f'{helpot_kysymykset[x][4]}\n    (a) {helpot_kysymykset[x][n[0]]}\n    (b) {helpot_kysymykset[x][n[1]]}\n    (c) {helpot_kysymykset[x][n[2]]} \n    (d) {helpot_kysymykset[x][n[3]]}')
+                print(n, m)
+                if oljenkorsi1 >0:
+                    print('(1) Oljenkorsi 1: Poista 1 väärä vastaus')
+                    
+                if oljenkorsi2 >0:
+                    
+                    print('(2) Oljenkorsi 2: Poista 2 väärää vastausta')
+                
+                valinta = input('Vastauksesi: ')
+                if valinta == 'a':
+                    print('Valitsit vaihtoehdon a\n')
+                    break
+                elif valinta == 'b':
+                    print('Valitsit vaihtoehdon b\n')
+                    break
+                elif valinta == 'c':
+                    print('Valitsit vaihtoehdon c\n')
+                    break
+                elif valinta == 'd':
+                    print('Valitsit vaihtoehdon d\n')
+                    break
+                elif valinta == '1':
+                
+                    oljenkorsi1 -=1
+                elif valinta == '2':
+                
+                    oljenkorsi2 -=1
+                
+                else:
+                    print('Virheellinen valinta\n')
+            except ValueError:
+                print('Virheellinen valinta\n')            
+
+if __name__ == '__main__':
+    visa_valikko()
 '''
