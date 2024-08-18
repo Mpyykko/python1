6d4827297dceca791deac3d0a038118ed1f89e10
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2024-08-18 19:53:06 +0300

    Tietovisaa, kysymykset omiin tiedostoihin, oljenkorsien logiikkaa

--- tietovisa.py
+++ tietovisa.py
@@ -1,107 +1,78 @@
-def visa_valikko():
-    oljenkorsi1 = 1
-    oljenkorsi2 = 1
-    print()
-    while True:
-        try:
-            print('Tähän tulee kysymys?\n    (a) Vastaus1\n    (b) Vastaus2\n    (c) Vastaus3 \n    (d) Vastaus4')
-            print()
-            if oljenkorsi1 >0:
-                print('(1) Oljenkorsi 1: Poista 1 väärä vastaus')
-                
-            if oljenkorsi2 >0:
-                print('(2) Oljenkorsi 2: Poista 2 väärää vastausta')
-               
-            valinta = input('Vastauksesi: ')
-            if valinta == 'a':
-                print('Valitsit vaihtoehdon a\n')
-                break
-            elif valinta == 'b':
-                print('Valitsit vaihtoehdon b\n')
-                break
-            elif valinta == 'c':
-                print('Valitsit vaihtoehdon c\n')
-                break
-            elif valinta == 'd':
-                print('Valitsit vaihtoehdon d\n')
-                break
-            elif valinta == '1':
-              
-                oljenkorsi1 -=1
-            elif valinta == '2':
-               
-                oljenkorsi2 -=1
-            
-            else:
-                print('Virheellinen valinta\n')
-        except ValueError:
-            print('Virheellinen valinta\n')
-
-
-
-if __name__ == '__main__':
-    visa_valikko()
-
-'''
+import json
 import random
 
-n = [0,1,2,3]
-m = []
-helpot_kysymykset = {
-    1:['Helsinki', 'Halsinki', 'Hulsinki', 'Joku muu', 'Suomen pääkaupunki on?'],
-    2:['Tapio Rautavaara', 'Reino Helismaa', 'Georg Otz', 'Kirka', 'Kuka oli tunnettu laulava keihäänheittäjä?'],
-    3:['Soisalo', 'Heisalo', 'Koisalo', 'Seisalo', 'Mikä suomalainen näyttelijä-ohjaaja Martti on sukuaan?'],
-    4:['3,14', '3,41', '4,13', '1,34', 'Piin likiarvo on?'],
-    5:['Simeoni', 'Eero', 'Aapo', 'Lauri', 'Kuka oli kolmanneksi vanhin Jukolan veljeksistä?']
-}
+
+def lataa_kysymykset(tiedosto):
+    with open(tiedosto, 'r', encoding='utf-8') as f:
+        kysymykset = json.load(f)
+    return kysymykset
 
 
 def visa_valikko():
-    x = random.randint(1, len(helpot_kysymykset))
+    oljenkorsi1 = True
+    oljenkorsi2 = True
+    pisteet = 0
+
+    helpot_kysymykset = lataa_kysymykset('kysymykset-helpot.json')
+    n = [0, 1, 2, 3]
+    m = []
+    
     for i in range(5):
+        x = random.randint(1, len(helpot_kysymykset))
         random.shuffle(n)
+        
         while x in m:
             x = random.randint(1, len(helpot_kysymykset))
         m.append(x)
-        oljenkorsi1 = 1
-        oljenkorsi2 = 1
         print()
+        
+        vastausvaihtoehdot = n.copy()
+        oikea_vastaus_idx = int(helpot_kysymykset[str(x)][-1])
+        
+        print(f'{helpot_kysymykset[str(x)][4]}')
+        
         while True:
             try:
-                print(f'{helpot_kysymykset[x][4]}\n    (a) {helpot_kysymykset[x][n[0]]}\n    (b) {helpot_kysymykset[x][n[1]]}\n    (c) {helpot_kysymykset[x][n[2]]} \n    (d) {helpot_kysymykset[x][n[3]]}')
-                print(n, m)
-                if oljenkorsi1 >0:
+                for idx, vaihtoehto in enumerate(vastausvaihtoehdot):
+                    print(f'    ({chr(97 + idx)}) {helpot_kysymykset[str(x)][vaihtoehto]}')
+                if oljenkorsi1:
                     print('(1) Oljenkorsi 1: Poista 1 väärä vastaus')
-                    
-                if oljenkorsi2 >0:
-                    
+                if oljenkorsi2:
                     print('(2) Oljenkorsi 2: Poista 2 väärää vastausta')
                 
-                valinta = input('Vastauksesi: ')
-                if valinta == 'a':
-                    print('Valitsit vaihtoehdon a\n')
-                    break
-                elif valinta == 'b':
-                    print('Valitsit vaihtoehdon b\n')
-                    break
-                elif valinta == 'c':
-                    print('Valitsit vaihtoehdon c\n')
-                    break
-                elif valinta == 'd':
-                    print('Valitsit vaihtoehdon d\n')
-                    break
-                elif valinta == '1':
+                valinta = input('Vastauksesi: ').strip().lower()
                 
-                    oljenkorsi1 -=1
-                elif valinta == '2':
+                if valinta in ['a', 'b', 'c', 'd']:
+                    if n[['a', 'b', 'c', 'd'].index(valinta)] == oikea_vastaus_idx:
+                        pisteet += 1
+                    else:
+                        print(f'Väärä vastaus! Saavutit {pisteet} pistettä\n')
+                        print('Peli loppui.')
+                        return 
+                    break
                 
-                    oljenkorsi2 -=1
+                elif valinta == '1' and oljenkorsi1:
+                    oljenkorsi1 = False
+                    vaarat_vaihtoehdot = [v for v in vastausvaihtoehdot if v != oikea_vastaus_idx]
+                    if vaarat_vaihtoehdot:
+                        poistettava = random.choice(vaarat_vaihtoehdot)
+                        vastausvaihtoehdot.remove(poistettava)
+                  
+            
+                elif valinta == '2' and oljenkorsi2:
+                    oljenkorsi2 = False
+                    vaarat_vaihtoehdot = [v for v in vastausvaihtoehdot if v != oikea_vastaus_idx]
+                    if len(vaarat_vaihtoehdot) >= 2:
+                        poistettavat = random.sample(vaarat_vaihtoehdot, 2)
+                        for poistettava in poistettavat:
+                            vastausvaihtoehdot.remove(poistettava)
+                
                 
                 else:
-                    print('Virheellinen valinta\n')
+                    print('Virheellinen valinta tai oljenkorsi on jo käytetty\n')
+            
             except ValueError:
-                print('Virheellinen valinta\n')            
+                print('Virheellinen valinta\n')
 
 if __name__ == '__main__':
     visa_valikko()
-'''
